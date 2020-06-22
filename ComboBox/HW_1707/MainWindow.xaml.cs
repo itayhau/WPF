9e4321fadc28390d91610140b8f8e4eef65a3509
itayhau <itayhau@gmail.com>
2020-06-22 15:02:10 +0300

    Add files via upload

---
+++ ComboBox/HW_1707/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_1707
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+
+            people.Add(new Person { FirstName = "Dana", LastName = "Koren" });
+            people.Add(new Person { FirstName = "Keren", LastName = "Koko" });
+            people.Add(new Person { FirstName = "Mor", LastName = "Mini" });
+
+            myComboBox.ItemsSource = people;
+            myComboBox.SelectedIndex = 0;
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse down");
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameTxt.Text}");
+            people.Add(new Person { FirstName = firstNameTxt.Text, LastName = "Doe" });
+        }
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ DataContext/DataContext/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataContext
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person MyPerson { get; set; }
+        public Person MyPerson2 { get; set; }
+        bool person1 = true;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            MyPerson = new Person { Name = "Very nice person!" };
+            MyPerson2 = new Person { Name = "very different person" };
+
+            //this.DataContext = MyPerson; { Binding Name }
+            this.DataContext = this; // { Binding MyPerson }
+            //this.lbl1.DataContext = MyPerson; // you can choose item for the DataContext
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"Person name {Name}";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (person1)
+            {
+                this.lbl1.DataContext = MyPerson2;
+            }
+            else
+            {
+                this.lbl1.DataContext = MyPerson;
+            }
+            person1 = !person1;
+        }
+    }
+
+}
